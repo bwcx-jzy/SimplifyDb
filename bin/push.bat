417c3d864d13c3c3f0bb5c9848aeefe61b738ec1
jiangzeyin <bwcx_jzy@163.com>
2018-09-22 14:35:42 +0800

    rem

--- bin/push.bat
+++ bin/push.bat
@@ -1,14 +1,14 @@
-
+rem 切换分支
 git checkout master
-
+rem 合并分支
 git merge dev
-
+rem 推送到主分支
 git push gitee master
-
+rem 合并 github master 分支
 git fetch github  master:master
-
+rem 推送到开发分支
 git push github dev
-
+rem  推送到主分支
 git push github master
-
+rem 切换到开发分支
 git checkout dev
