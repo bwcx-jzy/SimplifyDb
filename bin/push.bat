5fa17d0673656b87a11345a1a822cbc92b090b66
jiangzeyin <bwcx_jzy@163.com>
2018-09-22 14:55:27 +0800

    取消注释

--- bin/push.bat
+++ bin/push.bat
@@ -1,14 +1,14 @@
-% 切换分支 %
+
 git checkout master
-% 合并分支 %
+
 git merge dev
-% 推送到主分支 %
+
 git push gitee master
-% 合并 github master 分支 %
+
 git fetch github master:master
-% 推送到开发分支 %
+
 git push github dev
-%  推送到主分支 %
+
 git push github master
-% 切换到开发分支 %
+
 git checkout dev
