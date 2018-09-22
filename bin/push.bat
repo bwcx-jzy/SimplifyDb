1623e6ead515aecacfc16e36eef1eb95915a86f5
jiangzeyin <bwcx_jzy@163.com>
2018-09-22 14:49:05 +0800

    rem

--- bin/push.bat
+++ bin/push.bat
@@ -5,7 +5,7 @@ git merge dev
 % 推送到主分支 %
 git push gitee master
 % 合并 github master 分支 %
-git fetch github  master:master
+git fetch github master:master
 % 推送到开发分支 %
 git push github dev
 %  推送到主分支 %
